567ecd186a5702961144762d859ddd6cca35ae41
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-05-18 23:00:51 +0200

    Remove icmp hack, allow to specify src address.

--- nonxt-send.c
+++ nonxt-send.c
@@ -30,7 +30,7 @@
 void __dead
 usage(void)
 {
-	fprintf(stderr, "usage: nonxt-send addr\n");
+	fprintf(stderr, "usage: nonxt-send [srcaddr] dstaddr\n");
 	exit(1);
 }
 
@@ -40,85 +40,83 @@ main(int argc, char *argv[])
 	struct addrinfo hints, *res, *res0;
 	struct timeval to;
 	struct sockaddr_storage ss;
-	const char *cause = NULL;
+	const char *cause = NULL, *src, *dst;
 	socklen_t slen;
 	int error;
 	int save_errno;
 	int s, r, u;
 	char buf[1024];
-	int icmp;
 
-	if (argc != 2)
+	switch (argc) {
+	case 2:
+		src = NULL;
+		dst = argv[1];
+		break;
+	case 3:
+		src = argv[1]; 
+		dst = argv[2];
+		break;
+	default:
 		usage();
+	}
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_RAW;
 	hints.ai_protocol = IPPROTO_NONE;
-	error = getaddrinfo(argv[1], NULL, &hints, &res0);
+	error = getaddrinfo(dst, NULL, &hints, &res0);
 	if (error)
-		errx(1, "getaddrinfo: %s", gai_strerror(error));
+		errx(1, "getaddrinfo dst: %s", gai_strerror(error));
 
 	s = -1;
 	for (res = res0; res; res = res->ai_next) {
 		s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 		if (s == -1) {
-			cause = "socket nonxt";
+			cause = "socket";
 			continue;
 		}
 		if (connect(s, res->ai_addr, res->ai_addrlen) == -1) {
-			cause = "connect nonxt";
+			cause = "connect";
 			save_errno = errno;
 			close(s);
 			errno = save_errno;
 			s = -1;
 			continue;
 		}
-
-		/* connect udp socket and bind to local address */
-		u = socket(res->ai_family, SOCK_DGRAM, IPPROTO_UDP);
-		if (u == -1)
-			err(1, "socket udp");
-		memcpy(&ss, res->ai_addr, res->ai_addrlen);
-		if (res->ai_family == AF_INET)
-			((struct sockaddr_in *)&ss)->sin_port = htons(1);
-		if (res->ai_family == AF_INET6)
-			((struct sockaddr_in6 *)&ss)->sin6_port = htons(1);
-		if (connect(u, (struct sockaddr *)&ss, res->ai_addrlen) == -1)
-			err(1, "connect udp");
-		slen = sizeof(ss);
-		if (getsockname(u, (struct sockaddr *)&ss, &slen) == -1)
-			err(1, "getsockname udp");
-		if (res->ai_family == AF_INET)
-			((struct sockaddr_in *)&ss)->sin_port = htons(0);
-		if (res->ai_family == AF_INET6)
-			((struct sockaddr_in6 *)&ss)->sin6_port = htons(0);
-		if (close(u) == -1)
-			err(1, "close udp");
-
-		icmp = res->ai_family == AF_INET ? IPPROTO_ICMP :
-		    res->ai_family == AF_INET6 ? IPPROTO_ICMPV6 : 0;
-		r = socket(res->ai_family, SOCK_RAW, icmp);
-		if (r == -1)
-			err(1, "socket icmp");
-		if (bind(r, (struct sockaddr *)&ss, slen) == -1)
-			err(1, "bind icmp");
-		if (connect(r, res->ai_addr, res->ai_addrlen) == -1)
-			err(1, "connect icmp");
 		break;
 	}
-	if (s == -1)
+	if (res == NULL)
 		err(1, "%s", cause);
+
+	if (src != NULL) {
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = res->ai_family;
+		hints.ai_socktype = SOCK_RAW;
+		hints.ai_protocol = IPPROTO_NONE;
+		hints.ai_flags = AI_PASSIVE;
+		freeaddrinfo(res0);
+		error = getaddrinfo(src, NULL, &hints, &res0);
+		if (error)
+			errx(1, "getaddrinfo src: %s", gai_strerror(error));
+
+		for (res = res0; res; res = res->ai_next) {
+			if (bind(s, res->ai_addr, res->ai_addrlen) == -1)
+				continue;
+			break;
+		}
+		if (res == NULL)
+			err(1, "bind");
+	}
 	freeaddrinfo(res0);
 
 	if (send(s, buf, 0, 0) == -1)
-		err(1, "send nonxt");
+		err(1, "send");
 	to.tv_sec = 3;
 	to.tv_usec = 0;
 	if (setsockopt(r, SOL_SOCKET, SO_RCVTIMEO, &to, sizeof(to)) == -1)
-		err(1, "setsockopt icmp");
-	if (recv(r, buf, sizeof(buf), 0) == -1)
-		err(1, "recv icmp");
+		err(1, "setsockopt");
+	if (recv(s, buf, sizeof(buf), 0) == -1)
+		err(1, "recv");
 
 	return 0;
 }
